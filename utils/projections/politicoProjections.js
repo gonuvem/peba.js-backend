5ea86e3e3d5b045f5be62881aa5b5a07c9e2f67d
Edson Moita <moitaneto@hotmail.com>
2018-08-20 14:29:57 -0300

    Cria projeção de listagem de políticos.

---
+++ utils/projections/politicoProjections.js
@@ -0,0 +1,12 @@
+const politicoListProj = {
+  _id: true,
+  nome: true,
+  urlFoto: true,
+  siglaPartido: true,
+  siglaUf: true,
+  totalDespesas: true
+};
+
+module.exports = {
+  politicoListProj
+}
