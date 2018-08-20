19c233bb59de505d1d4f6ff629efd24d816dc08e
Edson Moita <moitaneto@hotmail.com>
2018-08-20 14:26:34 -0300

    Adiciona rota de políticos e error handler.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const restify = require('restify');
 const server = restify.createServer();
+const { handleErr } = require('./modules/errorHandler');
 
 // Inicar tarefas agendadas
 const { updatePoliticosJob } = require('./services/schedulerService');
@@ -8,9 +9,14 @@ updatePoliticosJob.start();
 // Requisitar rotas
 const deputadoRoute = require('./routes/DeputadoRoute');
 const despesaRoute = require('./routes/DespesaRoute');
+const politicoRoute = require('./routes/PoliticoRoute');
 
 // Montar rotas
 deputadoRoute.applyRoutes(server, '/deputados');
 despesaRoute.applyRoutes(server, '/despesas');
+politicoRoute.applyRoutes(server, '/politicos');
+
+server.use(restify.plugins.queryParser());
+server.on('restifyError', handleErr);
 
 module.exports = server;
