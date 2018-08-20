1655a7e7b254ea229ea004d9173351d2075d9028
Edson Moita <moitaneto@hotmail.com>
2018-08-20 20:06:15 -0300

    Adiciona schema para validação da requisição do endpoint de enviar email para político.

--- utils/schemas/baseSchemas.js
+++ utils/schemas/baseSchemas.js
@@ -3,5 +3,6 @@ const Joi = require('joi');
 module.exports = {
   idSchema: Joi.string().regex(/^[0-9a-fA-F]{24}$/).strict(),
   ufSchema: Joi.string().length(2).uppercase().strict(),
-  termsSchema: Joi.array().min(1).items(Joi.string().strict().required())
+  termsSchema: Joi.array().min(1).items(Joi.string().strict().required()),
+  emailSchema: Joi.string().email(),
 }
