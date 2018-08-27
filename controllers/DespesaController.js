533e88d15ff3d01f9d536a43d06a80c8db25d205
Edson Moita <moitaneto@hotmail.com>
2018-08-27 10:43:34 -0300

    Retira código de teste.

--- controllers/DespesaController.js
+++ controllers/DespesaController.js
@@ -1,29 +1,3 @@
 exports.test = async function (req, res, next) {
-    const { obterDeputadosV1, obterRelatorioDePresenca } = require('../services/coletorService');
-    const { getDeputadosMatriculas } =require('../services/parserService');
-    const x = await obterDeputadosV1();
-    const y = await getDeputadosMatriculas(x);
-    //const k = await matchDeputados(y)
-    const k = await obterRelatorioDePresenca(392);
-
-    return res.send(k);
     return res.send({ message: 'Teste do controller de despesas' });
-}
-
-const Politico = require('../models/PoliticoModel');
-
-async function matchDeputados(deputados) {
-    const deputadosDB = await Politico.find({ cargo: 'Deputado Federal' });
-
-    console.log('DB: ', deputadosDB.length, 'V1: ', deputados.length)
-
-    const codigosDB = deputadosDB.map(d => d.codigo);
-    //console.log(codigosDB)
-    const codigosV1 = deputados.map(d => d.matricula).sort((a,b) => a-b);
-    //console.log(codigosV1)
-    //const difference = codigosDB.filter(x => !codigosV1.includes(x));
-
-    //const erro = deputadosDB.map(codigosDB.filter(x => difference.includes(x)));
-
-    return codigosV1
 }
