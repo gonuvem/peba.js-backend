d37773cffec1815f63a758b4be7d06e83789d888
Edson Moita <moitaneto@hotmail.com>
2018-08-27 12:07:21 -0300

    Implementa endpoint que lista e cria termos no glossário. Cria rota e projeções. Monta rota no servidor.

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ const deputadoRoute = require('./routes/DeputadoRoute');
 const despesaRoute = require('./routes/DespesaRoute');
 const politicoRoute = require('./routes/PoliticoRoute');
 const mailerRoute = require('./routes/MailerRoute');
+const glossaryRoute = require('./routes/GlossaryRoute');
 
 // Montar rotas
 deputadoRoute.applyRoutes(server, '/deputados');
 despesaRoute.applyRoutes(server, '/despesas');
 politicoRoute.applyRoutes(server, '/politicos');
 mailerRoute.applyRoutes(server, '/email');
+glossaryRoute.applyRoutes(server, '/glossary');
 
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.queryParser());
---
+++ controllers/GlossaryController.js
@@ -0,0 +1,40 @@
+const Glossary = require('../models/GlossaryModel');
+const { glossaryListProj } =require('../utils/projections/glossaryProjections');
+const errs = require('restify-errors');
+
+exports.create = async function(req, res, next) {
+  try {
+    const { term, definition } = req.body;
+
+    const termFound = await Glossary.findOneAndUpdate({ term: term });
+    if (termFound) throw new errs.ConflictError('Termo já cadastrado');
+
+    const result = await Glossary
+    .create({ term: term, definition: definition });
+
+    return res.send(result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+exports.list = async function(req, res, next) {
+  try {
+    const { page = 0, perPage = 5 } = req.query;
+    
+    // Obter termos
+    const [ total, terms ] = await Promise.all([
+      Glossary.find({}).countDocuments(),
+      Glossary.find({}, glossaryListProj).sort('term')
+      .paginate(page, perPage)
+    ]);
+
+    // Calcular quantas páginas existem
+    const pages = Math.ceil(total / perPage);
+    
+    // Retornar os termos encontrados e os dados da paginação
+    return res.send({ terms: terms, total: total, pages: pages });
+  } catch (error) {
+    next(error);
+  } 
+}
