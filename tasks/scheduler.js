5342df2e72b21028cca8e97a347d3a6e21c755ef
Edson Moita <moitaneto@hotmail.com>
2018-08-31 12:05:25 -0300

    Retira o agendador de tarefas da pasta de services e separa numa pasta de tasks.

---
+++ tasks/scheduler.js
@@ -0,0 +1,80 @@
+const CronJob = require('cron').CronJob;
+const timezone = 'America/Fortaleza';
+const {
+  updateDeputadosTask, updateDeputadosTotalExpenditureTask,
+  updateDeputadosFrequency
+} = require('./camaraTasks');
+const {
+  updateSenadoresTask, updateSenadoresTotalExpenditureTask
+} = require('./senadoTasks');
+
+/**
+ * Cria um cron job que atualiza os políticos e seus totais de despesas.
+ * O cron job executa toda segunda-feira (1 no cronTime) às
+ * 2h da manhã (0 0 2 no cronTime), considerando a hora do fuso horário 
+ * America/Fortaleza.
+ */
+function updatePoliticiansJob() {
+  return new CronJob({
+    cronTime: '0 0 2 * * 1',
+    onTick: updateTask(),
+    timeZone: timezone,
+    start: false,
+    runOnInit: false,
+  });
+}
+/*
+const updatePoliticiansJob = new CronJob({
+  cronTime: '00 00 02 * * 1',
+  onTick: updateTask(),
+  timeZone: timezone
+});
+*/
+
+async function updateTask() {
+  try {
+    console.log('INICIANDO TAREFAS...');
+    console.log('Atualizar políticos...');
+    await updatePoliticiansTask();
+    console.log('Atualizar total de despesas...');
+    await updateTotalExpenditureTask();
+    console.log('Atualizar frequência dos políticos...');
+    await updatePoliticiansFrequencyTask(); 
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function updatePoliticiansTask() {
+  try {
+    await updateDeputadosTask();
+    await updateSenadoresTask();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateTotalExpenditureTask() {
+  try {
+    await updateDeputadosTotalExpenditureTask();
+    await updateSenadoresTotalExpenditureTask();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updatePoliticiansFrequencyTask() {
+  try {
+    await updateDeputadosFrequency(); 
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  updateTask,
+  updatePoliticiansTask,
+  updateTotalExpenditureTask,
+  updatePoliticiansFrequencyTask,
+  updatePoliticiansJob,
+}
