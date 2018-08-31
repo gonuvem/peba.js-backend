7d39990b07fe8a6cfb48272bc2f6456df15adabc
Edson Moita <moitaneto@hotmail.com>
2018-08-31 12:04:27 -0300

    Atualiza os testes para se adequar à nova organização do código.

---
+++ test/camaraParser.test.js
@@ -0,0 +1,95 @@
+const {
+  getDeputadosListV2, getDeputadoDetail, getDeputadoExpenses,
+  getDeputadosListV1, getDeputadoFrequency
+} = require('../services/camara/camaraColector');
+const {
+  getDeputadosIds, createPoliticiansFromDeputados, parseDeputadosExpenses,
+  getDeputadosTotalExpenditure, getDeputadosRegistration, parseFrequency
+} = require('../services/camara/camaraParser');
+
+const deputado = {
+  id: 178957,
+  nome: 'Abel Mesquita Jr.'
+}
+
+const deputado2 = {
+  mat: 312
+}
+
+describe('Tests Camara Parser Service', () => {
+
+  let deputadosListV2;
+  let deputadoDetail;
+  let deputadoExpenses;
+  let deputadoFrequency;
+  beforeAll(async () => {
+    deputadosListV2 = await getDeputadosListV2();
+    deputadoDetail = await getDeputadoDetail(deputado.id);
+    deputadoExpenses = await getDeputadoExpenses(deputado.id);
+    deputadosListV1 = await getDeputadosListV1();
+    deputadoFrequency = await getDeputadoFrequency(deputado2.mat);
+  });
+
+  describe('Tests getDeputadosIds', () => {
+    
+    test('More than 513 deputados', () => {
+      return getDeputadosIds(deputadosListV2).then(response => {
+        expect(response.length).toBeGreaterThanOrEqual(513)
+      });
+    });
+
+  });
+
+  describe('Tests createPoliticiansFromDeputados', () => {
+
+    test('Deputado Abel Mesquita', () => {
+      return createPoliticiansFromDeputados([deputadoDetail])
+      .then(response => {
+        expect(response[0].nome).toBe(deputado.nome)
+      });
+    });
+
+  });
+
+  describe('Tests parseDeputadosExpenses', () => {
+
+    test('Deputados Expenses', () => {
+      return parseDeputadosExpenses([deputadoExpenses]).then(response => {
+        expect(response[0].codigo).toBe(deputado.id)
+      });
+    });
+
+  });
+
+  describe('Tests getDeputadosTotalExpenditure', () => {
+
+    test('Deputados Total Expenditure', async () => {
+      const expenses = await parseDeputadosExpenses([deputadoExpenses])
+      return getDeputadosTotalExpenditure(expenses).then(response => {
+        expect(response[0].codigo).toBe(deputado.id)
+      });
+    });
+
+  });
+
+  describe('Tests getDeputadosRegistration', () => {
+
+    test('513 registrations', () => {
+      return getDeputadosRegistration(deputadosListV1).then(response => {
+        expect(response.length).toBe(513)
+      });
+    });
+
+  });
+
+  describe('Tests parseFrequency', () => {
+
+    test('Registration number', () => {
+      return parseFrequency(deputadoFrequency).then(response => {
+        expect(response.matricula).toBe(deputado2.mat)
+      });
+    });
+
+  });
+
+});
--- test/utils.test.js
+++ test/utils.test.js
@@ -57,3 +57,70 @@ describe.only('Testar utils', () => {
   });
 
 });
+
+/*
+const sampleXml = `
+<dados>
+    <deputado_>
+      <id>178957</id>
+      <uri>https://dadosabertos.camara.leg.br/api/v2/deputados/178957</uri>
+      <nome>ABEL MESQUITA JR.</nome>
+      <siglaPartido>DEM</siglaPartido>
+      <uriPartido>https://dadosabertos.camara.leg.br/api/v2/partidos/36769</uriPartido>
+      <siglaUf>RR</siglaUf>
+      <idLegislatura>55</idLegislatura>
+      <urlFoto>http://www.camara.leg.br/internet/deputado/bandep/178957.jpg</urlFoto>
+    </deputado_>
+</dados>    
+`;
+
+describe('Testar Parser Service', () => {
+
+  describe('Testar validarXml', () => {    
+
+    test('Arquivo vazio', () => {
+      return validarXml('').catch(e =>
+        expect(e).toEqual(new Error('Não é um arquivo XML')));
+    });
+
+    test('Arquivo inválido', () => {
+      return validarXml('String não xml').catch(e =>
+        expect(e).toEqual(new Error('Não é um arquivo XML')));
+    });
+
+    test('Arquivo é um xml', () => {
+      return validarXml(sampleXml).then(response =>
+        expect(response).toBe(true));
+    });
+
+  });
+
+  describe('Testar converterXmlParaJson', () => {
+    
+    test('Arquivo vazio', () => {
+      return converterXmlParaJson('').catch(e => 
+        expect(e).toEqual(new Error('Não é um arquivo XML')))
+    });
+
+    test('Arquivo inválido', () => {
+      return converterXmlParaJson('String não xml').catch(e => 
+        expect(e).toEqual(new Error('Não é um arquivo XML')))
+    });
+
+    test('Conversão bem sucedida', () => {
+      return converterXmlParaJson(sampleXml).then(response =>
+        expect(response).toMatchObject({
+          dados: {
+            deputado_: {
+              id: 178957,
+              nome: 'ABEL MESQUITA JR.'
+            }
+          }
+        }));
+    });
+
+  });
+
+});
+
+*/
