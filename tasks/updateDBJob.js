10febac7a47512287aa426e047b9635b837b8f32
Edson Moita <moitaneto@hotmail.com>
2018-09-14 15:48:02 -0300

    Corrige a url do BD.

--- tasks/updateDBJob.js
+++ tasks/updateDBJob.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { updateTask } = require('./scheduler');
 
-mongoose.connect('mongodb://db:27017/pebatest', { useNewUrlParser: true });
-
-updateTask();
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+.then(updateTask())
+.then(console.log('Tarefa Iniciada'))
+.catch(error => console.log(error))
