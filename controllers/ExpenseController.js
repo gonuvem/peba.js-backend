d366cbee03aa5c027dcf2636c555a37a9c9421df
Edson Moita <moitaneto@hotmail.com>
2018-09-15 19:33:04 -0300

    Adiciona função getChartsData que retorna informações dos gráficos.

--- controllers/ExpenseController.js
+++ controllers/ExpenseController.js
@@ -1,11 +1,15 @@
 const Expense = require('../models/ExpenseModel');
 const { expenseListProj } = require('../utils/projections/expenseProjections');
+const {
+  totalizeExpensesByMonth, totalizeExpensesByType, 
+  totalizeExpensesByTopNProviders
+} = require('../helpers/expenseHelper');
 
 exports.getExpenses = async function (req, res, next) {
   // Obter parâmetros da requisição
   const { page = 0, perPage = 5, ...query } = req.query;
 
-  // Encontrar as despesas do deputado de acordo com os parâmetros e a página requisitada
+  // Obter as despesas do político conforme os parâmetros e a página
   const [ total, expenses ] = await Promise.all([
     Expense.find(query, expenseListProj).countDocuments(),
     Expense.find(query, expenseListProj).paginate(page, perPage).sort('-date')
@@ -22,4 +26,24 @@ exports.getExpenses = async function (req, res, next) {
 
   res.status(200)
   return res.send({ message: 'Despesas não encontradas' });
+}
+
+exports.getChartsData = async function(req, res, next) {
+  
+  // Obter parâmetros da requisição
+  const { politicianId } = req.query;
+
+  // Obter as despesas do político conforme os parâmetros
+  const expenses = await Expense.find({ politicianId: politicianId });
+  
+  // Obter os dados dos gráficos
+  const [ expensesByMonth, expensesByType, expensesByTopNProviders ] =
+  await Promise.all([
+    totalizeExpensesByMonth(expenses),
+    totalizeExpensesByType(expenses),
+    totalizeExpensesByTopNProviders(expenses)
+  ]);
+  
+  return res.send({
+    expensesByMonth, expensesByType, expensesByTopNProviders });
 }
