49055df666352560a0b2d68817883314589aceb0
Edson Moita <moitaneto@hotmail.com>
2018-09-15 19:34:39 -0300

    Cria testes para o endpoint /expenses/charts.

--- test/controllers/ExpenseController.test.js
+++ test/controllers/ExpenseController.test.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 // Rotas
 const baseRoute = '/expenses';
+const getChartsDataRoute = `${ baseRoute }/charts`;
 const idNotFound = '9b7419fcfc38c7964a15c287'
 
 describe('Test Expense Controller', () => {
@@ -18,14 +19,41 @@ describe('Test Expense Controller', () => {
     });
 
     test('400 PoliticianId invalid format', () => {
-      return request(app).get(baseRoute).query({ politicianId: 'abc' }).send()
+      return request(app).get(baseRoute).query({ politicianId: 'abc' })
+      .send().then(response => {
+        expect(response.statusCode).toBe(400)
+      })
+    });
+
+    test('200 Empty Expenses', () => {
+      return request(app).get(baseRoute).query({ politicianId: idNotFound })
+      .send().then(response => {
+        expect(response.statusCode).toBe(200)
+      })
+    });
+
+  });
+
+  describe('GET /expenses/charts', () => {
+
+    test('400 Missing politicianId', () => {
+      return request(app).get(getChartsDataRoute).query().send()
+        .then(response => {
+          expect(response.statusCode).toBe(400)
+        })
+    });
+
+    test('400 PoliticianId invalid format', () => {
+      return request(app).get(getChartsDataRoute)
+      .query({ politicianId: 'abc' }).send()
         .then(response => {
           expect(response.statusCode).toBe(400)
         })
     });
 
     test('200 Empty Expenses', () => {
-      return request(app).get(baseRoute).query({ politicianId: idNotFound }).send()
+      return request(app).get(getChartsDataRoute)
+      .query({ politicianId: idNotFound }).send()
         .then(response => {
           expect(response.statusCode).toBe(200)
         })
